9435619f4ffb15880ad9a20705b08dfdfa52bf54
Teo Wen Long <wenlong.teo@qumonintelligence.com>
2022-08-07 22:01:48 +0800

    test123

--- src/pages/admin/mimi.tsx
+++ src/pages/admin/mimi.tsx
@@ -2,20 +2,17 @@ import React from "react";
 import { withLayout } from "@moxy/next-layout";
 import Admin from ".";
 import PageHeader from "../../components/layouts/Admin/page-header";
-import { Button } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 
 const Mimi = () => {
   return (
-    <>
-      <PageHeader>
-        <PageHeader.Title>米米</PageHeader.Title>
-        <PageHeader.Actions>
-          <Button>
-            TESTTT
-          </Button>
-        </PageHeader.Actions>
-      </PageHeader>
-    </>
+    <Box>
+      <Heading>
+      米米
+    </Heading>
+    <Text>DADSDAD</Text>
+    <Button>DASDASDDDAzzzz</Button>
+    </Box>
   );
 };
 
