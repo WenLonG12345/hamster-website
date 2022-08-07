554fe8989cf9fb951a58298b7f7c78625bc64951
Teo Wen Long <wenlong.teo@qumonintelligence.com>
2022-08-07 21:59:17 +0800

    test

--- src/pages/admin/mimi.tsx
+++ src/pages/admin/mimi.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 import { withLayout } from "@moxy/next-layout";
-import Admin from '.';
+import Admin from ".";
+import PageHeader from "../../components/layouts/Admin/page-header";
+import { Button } from "@chakra-ui/react";
 
 const Mimi = () => {
   return (
-    <div>Mimi</div>
-  )
-}
+    <>
+      <PageHeader>
+        <PageHeader.Title>米米</PageHeader.Title>
+        <PageHeader.Actions>
+          <Button>
+            TESTTT
+          </Button>
+        </PageHeader.Actions>
+      </PageHeader>
+    </>
+  );
+};
 
-export default withLayout(<Admin/>)(Mimi);
+export default withLayout(<Admin />)(Mimi);
